65e7888139530634ddc40b730fbf2470a04e3df4
Tyler Daerden <daerdentyler@gmail.com>
2023-06-22 16:46:45 +0200

    Mise en Place DB avec User only et register pour le début

---
+++ Gest_ContactsDatabase/Procedures/CSP_Register.sql
@@ -0,0 +1,15 @@
+﻿-- Le non est CSP pour (Contact Stored Procedure) un autre nom était acceptable of course --
+
+CREATE PROCEDURE [dbo].[CSP_Register]
+	@Email NVARCHAR(384),
+	@Passwd BINARY(64)
+AS
+BEGIN
+/*
+	Petit test des valeurs
+*/
+	SELECT Id,Nom,Prenom, @Email AS Email
+	FROM Utilisateur
+	WHERE Email = @Email AND Passwd = HASHBYTES( 'SHA2_512', CONCAT( dbo.CSF_GetPreSalt(),@Passwd,dbo.CSF_GetPostSalt() ) );
+RETURN 0
+END
